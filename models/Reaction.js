1979369f42bd8f0ec8234f45fe210db7002c6dfb
adamjfox <adamjeffersonfox@gmail.com>
2023-07-27 17:10:35 -0700

    Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const ReactionModel = mongoose.model('Reaction', reactionSchema);
+
+module.exports = ReactionModel;
